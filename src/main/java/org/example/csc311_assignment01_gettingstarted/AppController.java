14d34b654e825cc1b535840d3d03c56124b15659
mand33 <amandaharnos5@gmail.com>
2026-02-05 21:21:58 -0500

    #FINAL COMMIT

--- src/main/java/org/example/csc311_assignment01_gettingstarted/AppController.java
+++ src/main/java/org/example/csc311_assignment01_gettingstarted/AppController.java
@@ -3,10 +3,10 @@ package org.example.csc311_assignment01_gettingstarted;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-import java.awt.*;
+import javafx.scene.control.TextField;
 
 public class AppController {
-    @FXML
+   @FXML
     private Label welcomeText;
 
     // ToDo 01: add two text fields to the GUI so that the user can find the sum
@@ -18,16 +18,27 @@ public class AppController {
 
 
     // ToDo 02: when the button is clicked show the output on the screen
+    @FXML protected void onCalculateButtonClick() {
+        // ToDo 03: you should accept only the numeric values
+        try{
+            //get text and convert to double
+            double num1 = Double.parseDouble(input1.getText());
+            double num2 = Double.parseDouble(input2.getText());
+            double result = num1 + num2;
+            //display result
+            welcomeText.setText("Sum: " + result);
+        }
+        catch (NumberFormatException e){
+            //if types in letters
+            welcomeText.setText("Only accepts number values.");
+        }
+    }
 
-    // ToDo 03: you should accept only the numeric values
 
     // ToDo 04: modify the CSS file to make the background of the application Lightblue and the button color red
 
     // ToDo 05: commit changes and push back to the same repo
 
 
-    @FXML
-    protected void onCalculateButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
 }
